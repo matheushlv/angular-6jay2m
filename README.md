040a6f8421d68021addea5a3cc8b2e20ef1b1dc2
matheushlv <matheus.henrique.25@hotmail.com>
2020-12-18 13:02:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6jay2m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6jay2m)
---
+++ src/app/turma.component.ts
@@ -0,0 +1,13 @@
+
+
+@Component({
+  selector: 'turma-component',
+  templateUrl: 'datepicker-moment-example.html',
+  providers: [
+    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+    // `MatMomentDateModule` in your applications root module. We provide it at the component level
+    // here, due to limitations of our example generation script.
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
+})
